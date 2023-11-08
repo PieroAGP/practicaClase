f2f81d3cacb54f7846fa64acbfcb3636902012eb
PieroAGP <xpgabpar357@ieshnosmachado.org>
2023-11-08 10:26:09 +0100

    creaccion de formulario

--- task/views.py
+++ task/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Task
+from .forms import TaskForm
 # Create your views here.
 
 def listaTareas(request):
     tareas = Task.objects.all()
-    return render(request,'task/listaTareas.html',{'tareas': tareas})
+    if request.method == 'POST':
+      form = Task
+
+      return redirect('listaTareas')  
+
+    else:
+       form= TaskForm()
+    return render(request,'task/listaTareas.html',{'tareas': tareas,'form':form})
 
